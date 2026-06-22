1cb9fc9ec2bffa0c8bb6ba2a1249e9563c19046d
sindhiya <support@letzshopy.in>
2026-06-22 22:49:44 +0530

    customer feedback v3

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,19 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // Allow WordPress uploads from your sites (adjust if needed)
       { protocol: "https", hostname: "letzshopy.in" },
       { protocol: "https", hostname: "www.letzshopy.in" },
       { protocol: "https", hostname: "template.letzshopy.in" },
-      // Add any CDN/Jetpack domain you use for images
+      { protocol: "https", hostname: "7pleats.letzshopy.in" },
       { protocol: "https", hostname: "**.wp.com" },
     ],
   },
 
-  
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
