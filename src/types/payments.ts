4ceb3bc6cc23eda4a53e186c9dcb1f69002aa38d
letzshopy <support@letzshopy.in>
2026-09-24 09:01:31 +0530

    Replace Easebuzz payment type with PayGlocal

--- src/types/payments.ts
+++ src/types/payments.ts
@@ -4,14 +4,9 @@ export interface PaymentsFormValues {
     default_status: string;
   };
 
-  easebuzz: {
+  payglocal: {
     enabled: boolean;
-    mode?: string;
-    merchant_key?: string;
-    salt?: string;
-    merchant_id?: string;
-    webhook_secret?: string;
-    hint?: string;
+    gateway_id?: string;
   };
 
   upi: {
